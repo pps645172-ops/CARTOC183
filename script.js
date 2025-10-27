939d459285ec1861a6b38e9d58aa9afb2907045d
pps645172-ops <pps645172@berkeley.edu>
2025-10-27 10:32:50 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoic29waGlhbWV5ZXJzIiwiYSI6ImNtaDljY2V5dzFkYm0ya
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/sophiameyers/cmh9dpvwu00ay01sq35bb44mx', // your Style URL goes here
-  center: [-122.27, 37.87], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+  center: [-122.6784, 45.5152], // starting position [lng, lat]. Note that lat must be set between -90 and 90
   zoom: 9 // starting zoom
     });
