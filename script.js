fae403315a21a610bbcb14e281a9eb12803245b9
pps645172-ops <pps645172@berkeley.edu>
2025-10-27 10:00:14 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic29waGlhbWV5ZXJzIiwiYSI6ImNtaDljY2V5dzFkYm0ya3B0cTB5NGxjYXUifQ.BKtiD06UgJVUyv1ODAQ-vA';
+const map = new mapboxgl.Map({
+  container: 'map', // container ID
+  style: 'mapbox://styles/sophiameyers/cmh9dpvwu00ay01sq35bb44mx', // your Style URL goes here
+  center: [-122.27, 37.87], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+  zoom: 9 // starting zoom
+    });
